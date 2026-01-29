0d9d51e8f09d7e008d9b713c19711091909c8cc7
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-01-29 17:09:50 +0530

    Fix in Main Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+        System.out.println("Hello and welcome!");
         ArrayAndArrayList.arrayDemo();
         ArrayAndArrayList.arrayListDemo();
 
