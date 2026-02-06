d4025c686e92985881a1d7e194b0739a630d7bcc
KumarSurajwebdeveloper <kr.surajkumar09@gmail.com>
2026-02-06 23:25:51 +0530

    Separating DSA important structures

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,18 +1,23 @@
 package main;
 
 import collections.*;
+import collections.ArrayAndArrayList;
+import collections.HashMapMethodsDemo;
+import collections.HashSetBulkOperations;
+import collections.HashSetMethods;
+import collections.ListMethods;
 
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("Hello and welcome!");
-//        ArrayAndArrayList.arrayDemo();
-//        ArrayAndArrayList.arrayListDemo();
-//        ListMethods.runListMethodsDemo();
-//        CopyOnWriteArrayListDemo.runCopyOnWriteArrayListDemo();
-//        LinkedListMethods.runLinkedListMethodsDemo();
-//        HashMapMethodsDemo.runHashMapDemo();
+        ArrayAndArrayList.arrayDemo();
+        ArrayAndArrayList.arrayListDemo();
+        ListMethods.runListMethodsDemo();
+        CopyOnWriteArrayListDemo.runCopyOnWriteArrayListDemo();
+        LinkedListMethods.runLinkedListMethodsDemo();
+        HashMapMethodsDemo.runHashMapDemo();
         HashSetMethods.runHashSetMethodsDemo();
         HashSetBulkOperations.runBulkOperationsDemo();
 
